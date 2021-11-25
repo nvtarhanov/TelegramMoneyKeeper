445ea612249cab872fb73b05148d4d966aa3f99c
nvtarhanov <tnv@sitec-it.ru>
2021-11-25 22:37:07 +0400

    Add more test

--- repository/state_test.go
+++ repository/state_test.go
@@ -42,3 +42,57 @@ func (s *Suite) Test_GetCurrentStateByIDError() {
 	assert.NotNil(s.T(), state)
 	assert.Equal(s.T(), state, 0)
 }
+
+func (s *Suite) Test_WriteState() {
+
+	testData := testState{id: 1, state: 1}
+
+	query := regexp.QuoteMeta(`INSERT INTO "states" ("state","id") VALUES ($1,$2) RETURNING "id"`)
+
+	s.mock.ExpectBegin()
+
+	s.mock.ExpectQuery(query).WithArgs(testData.id, testData.id).WillReturnRows(s.mock.NewRows([]string{"id"}).AddRow("1"))
+
+	s.mock.ExpectCommit()
+
+	err := s.StateRepository.WriteState(testData.id, testData.state)
+
+	assert.NoError(s.T(), err)
+
+}
+
+func (s *Suite) Test_WriteStateError() {
+
+	testData := testState{id: 1, state: 1}
+
+	query := regexp.QuoteMeta(`INSERT INTO "states" ("state","id") VALUES ($1,$2) RETURNING "id"`)
+
+	s.mock.ExpectBegin()
+
+	s.mock.ExpectQuery(query).WithArgs(testData.id, testData.id).WillReturnError(errors.New("Some error"))
+
+	s.mock.ExpectRollback()
+
+	err := s.StateRepository.WriteState(testData.id, testData.state)
+
+	assert.Error(s.T(), err)
+
+}
+
+func (s *Suite) Test_UpdateState() {
+
+	testData := testState{id: 1, state: 1}
+
+	query := regexp.QuoteMeta(`INSERT INTO "states" ("state","id") VALUES ($1,$2) RETURNING "id"`)
+
+	s.mock.ExpectBegin()
+
+	s.mock.ExpectQuery(query).WithArgs(testData.id, testData.id).WillReturnError(errors.New("Some error"))
+
+	s.mock.ExpectRollback()
+
+	err := s.StateRepository.WriteState(testData.id, testData.state)
+
+	assert.Error(s.T(), err)
+
+}
